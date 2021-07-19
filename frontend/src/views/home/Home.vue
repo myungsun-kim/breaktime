721f92a86d55f9a0178657ae9f5907f02fade429
권순현 <ssafy5552@gmail.com>
2021-07-19 17:12:01 +0900

    frontcode

---
+++ frontend/src/views/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>쉬는시간</h1>
+  <Login />
+</template>
+
+<script>
+import Login from './components/Login.vue'
+export default {
+  name: 'Home',
+  components: {
+    Login,
+  },
+  setup() {
+    
+  },
+}
+</script>
