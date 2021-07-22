e678bf77d578b3e692bd13525e9de34d7d0d692d
박범진 <qjawlsqjacks@naver.com>
2021-07-23 03:30:13 +0900

    회의방 생성 기능

--- backend/src/main/java/com/ssafy/api/controller/ConferenceController.java
+++ backend/src/main/java/com/ssafy/api/controller/ConferenceController.java
@@ -1,6 +1,8 @@
 package com.ssafy.api.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,35 +11,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.api.request.ConferenceVO;
 import com.ssafy.api.service.ConferenceService;
 import com.ssafy.common.model.response.BaseResponseBody;
 import com.ssafy.db.entity.Conference;
+import com.ssafy.db.entity.ConferenceCategory;
+
+import lombok.RequiredArgsConstructor;
 
 // 회의방 기능 컨트롤러
 @RestController
 @RequestMapping("/conference")
+@RequiredArgsConstructor
 public class ConferenceController {
 	
 	@Autowired
 	ConferenceService conferenceService;
 	
 	@PostMapping("/make")
-	public ResponseEntity<? extends BaseResponseBody> make(Conference input){
-		System.out.println(input.toString());
-		Conference conference = new Conference();
-		conference.setConferenceCategory(input.getConferenceCategory());
-		conference.setDescription(input.getDescription());
-		conference.setName(input.getName());
-		conference.setOwner(input.getOwner());
-		conference.setParticipantLimit(input.getParticipantLimit());
-		conference.setPassword(input.getPassword());
-		conference.setProduceTime(input.getProduceTime());
+	public ResponseEntity<? extends BaseResponseBody> make(@RequestBody ConferenceVO confer){
 		
+		Conference conference = new Conference();
+		
+		ConferenceCategory category = new ConferenceCategory(); 
+		category.setSequence(confer.getCategory_seq()); // 회의방의 카테고리 컬럼을 카테고리 테이블 기본키랑 연결하기 위함
+		
+		conference.setConferenceCategory(category);
+		conference.setDescription(confer.getDescription());
+		conference.setName(confer.getName());
+		conference.setOwner(confer.getOwner());
+		conference.setParticipantLimit(confer.getParticipantLimit());
+		conference.setPassword(confer.getPassword());
+		
+		conference.setProduceTime(LocalDateTime.now()); // 현재 시간
 		
 		Long seq = conferenceService.create(conference);
-
+		
 		if(seq != null) {
 			return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
 		}else {
