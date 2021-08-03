08e97702464bfcd8da49dc0956ec90a4356f8ff6
김명선 <kms8072@daum.net>
2021-08-03 15:11:19 +0900

    회의방 참가자 Service

---
+++ backend/src/main/java/com/ssafy/api/service/ConferenceParticipantService.java
@@ -0,0 +1,23 @@
+package com.ssafy.api.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.db.entity.ConferenceParticipant;
+import com.ssafy.db.repository.ConferenceParticipantRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ConferenceParticipantService {
+	private final ConferenceParticipantRepository conferenceParticipantRepository;
+	
+	// 회의방 참가 후 참가자 번호 리턴
+	@Transactional(readOnly = false)
+	public Long create(ConferenceParticipant conferenceParticipant) {
+		conferenceParticipantRepository.save(conferenceParticipant);
+		return conferenceParticipant.getSequence();
+	}
+}
