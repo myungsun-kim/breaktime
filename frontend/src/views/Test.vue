7e5bf9d38fd03278590d305584d522bc7986079d
박범진 <qjawlsqjacks@naver.com>
2021-08-05 14:44:55 +0900

    temp

--- frontend/src/views/Test.vue
+++ frontend/src/views/Test.vue
@@ -25,61 +25,81 @@ import Stomp from 'webstomp-client'
 import SockJS from 'sockjs-client'
 
 export default {
-  name: 'Test',
+  name: 'ChatSide',
+
   data() {
     return {
-      userName: "",
       message: "",
-      recvList: []
-    }
+      messages: [],
+      participants: [],
+      userId: '',
+    };
   },
-  created() {
-    // App.vue가 생성되면 소켓 연결을 시도합니다.
-    this.connect()
-  },
-  methods: {
-    sendMessage (e) {
-      if(e.keyCode === 13 && this.userName !== '' && this.message !== ''){
-        this.send()
-        this.message = ''
-      }
-    },    
-    send() {
-      console.log("Send message:" + this.message);
-      if (this.stompClient && this.stompClient.connected) {
-        const msg = { 
-          userName: this.userName,
-          content: this.message 
-        };
-        this.stompClient.send("/receive", JSON.stringify(msg), {});
-      }
-    },    
-    connect() {
-      const serverURL = "http://localhost:8080"
-      let socket = new SockJS(serverURL);
-      this.stompClient = Stomp.over(socket);
-      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
-      this.stompClient.connect(
-        {},
-        frame => {
-          // 소켓 연결 성공
-          this.connected = true;
-          console.log('소켓 연결 성공', frame);
-          // 서버의 메시지 전송 endpoint를 구독합니다.
-          // 이런형태를 pub sub 구조라고 합니다.
-          this.stompClient.subscribe("/send", res => {
-            console.log('구독으로 받은 메시지 입니다.', res.body);
 
-            // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
-            this.recvList.push(JSON.parse(res.body))
+  props: {
+    roomId : String,
+  },
+
+  created() {
+    this.userId = this.getUserId;
+    console.log('아이디:'+this.userId);
+    console.log('방번호:'+this.roomId);
+
+    this.connect();
+    this.connection();
+    this.register();
+  },
+
+  destroyed() {
+    this.leaveRoom();
+  },
+
+  computed: {
+    ...mapGetters('user', ['getUserId']),
+  },
+
+  methods: {
+    // WebSocket
+    sendMessage() {
+      this.ws.send(
+        "/pub/chat/message",
+        JSON.stringify({
+          type: "TALK",
+          roomId: this.roomId,
+          sender: this.userId,
+          message: this.message,
+        }),
+        {}
+      );
+      this.message = "";
+    },
+    recvMessage(recv) {
+      this.messages.unshift({
+        type: recv.type,
+        sender: recv.type == "ENTER" ? "[알림]" : recv.sender,
+        message: recv.message,
+      });
+    },
+    connect() {
+      // this.ws = new Stomp.over(new SockJS("http://3.35.171.221:8443/ws-stomp));
+      this.ws = new Stomp.over(new SockJS("http://localhost:8443/ws-stomp));
+      var app = this;
+
+      this.ws.connect({}, function () {
+          app.ws.subscribe("/sub/chat/room/" + app.roomId, function (message) {
+            var recv = JSON.parse(message.body);
+            app.recvMessage(recv);
           });
+          app.ws.send("/pub/chat/message",
+            JSON.stringify({
+              type: "ENTER",
+              roomId: app.roomId,
+              sender: app.userId,
+            }),
+            {}
+          );
         },
-        error => {
-          // 소켓 연결 실패
-          console.log('소켓 연결 실패', error);
-          this.connected = false;
-        }
-      );        
+      );
     }
   }
 }
