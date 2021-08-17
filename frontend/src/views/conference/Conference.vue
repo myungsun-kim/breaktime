aee0f53a8fa075ac7c67c6999eb007020f87179c
DESKTOP-KVCQHCD\multicampus <hwangkj14@naver.com>
2021-08-17 13:03:03 +0900

    roomcard, conference_header , CSS

--- frontend/src/views/conference/Conference.vue
+++ frontend/src/views/conference/Conference.vue
@@ -1,9 +1,24 @@
 <template>
   <el-container>
+
     <el-header>
-      <div class="logo-breaktime">
-        <img src="../../assets/logo_cat.png" alt="" class="logo">
-        <img src="../../assets/breaktime.png" alt="" class="breaktime">
+      <div class="conference-header">
+
+        <div class="logo-breaktime">
+          <img src="../../assets/logo_cat.png" alt="" class="logo">
+          <img src="../../assets/breaktime.png" alt="" class="breaktime">
+        </div>
+
+        <div>
+          
+          <p class="title">Room : {{name}}</p>
+        </div>
+
+        <div class="room-owner">
+          <p>방번호: {{conferenceId}} </p>
+          <p>방 장: {{owner}} </p>
+        </div>
+
       </div>
     </el-header>
 
@@ -18,12 +33,14 @@
         <chat-side :conferenceId="props.conferenceId"/>
       </el-aside>
     </el-container>
+
   </el-container>
 </template>
 
 <script>
 import ChatSide from './components/ChatSide.vue'
 import ConferenceMain from './components/ConferenceMain.vue'
+
 
 export default {
   name: 'Conference',
@@ -40,15 +57,31 @@ export default {
       type: String,
       default: ''
     },
+    name: {
+      type: String,
+      default: ''
+    }
   },
   setup(props) {
 
     return {props}
   }
 }
+
 </script>
 
 <style scoped>
+
+  .conference-header {
+    font-family: 'Jua', sans-serif;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    /* margin-left: 20px; */
+    border-bottom: 1px solid;
+  }
+
+
   .conference-box {
     height: 100%;
   }
@@ -59,6 +92,13 @@ export default {
 
   .logo-breaktime {
     margin-left: 20px;
-    border-bottom: 1px solid;
+    /* border-bottom: 1px solid; */
   }
+
+  .room-owner {
+    margin-right: 40px;
+    text-align: left;
+  }
+
+
 </style>
