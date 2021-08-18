ae0bd29a20a5e1dcfc52627adf165c9f61353e49
박범진 <qjawlsqjacks@naver.com>
2021-08-18 12:00:07 +0900

    service

--- backend/src/main/java/com/ssafy/api/service/ConferenceService.java
+++ backend/src/main/java/com/ssafy/api/service/ConferenceService.java
@@ -2,7 +2,6 @@ package com.ssafy.api.service;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
